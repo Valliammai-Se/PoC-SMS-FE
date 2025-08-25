4514de2152826870dde0cac00ff5b5261934c10d
Valliammai-Se <valliammai.se@rootquotient.com>
2025-08-25 18:21:42 +0530

    Delete all code from repo

--- web/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import CustomerList from "./components/CustomerList";
-
-const App: React.FC = () => {
-  return <CustomerList />;
-};
-
-export default App;
--- web/src/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Customer {
-    id: number;
-    name: string;
-    phone: string;
-    email: string;
-    chatHistory: string[];
-  }
-  
